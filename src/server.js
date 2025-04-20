b15a69b76229c42df0a959fc5d121f4f3904bca0
Heloise Santos <heloise.hssantos@gmail.com>
2025-04-20 11:40:55 -0300

    configura .env para variáveis de ambiente

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
+
 const mongoose = require("mongoose");
 const app = require("./app");
 
 const port = 3001;
 
 mongoose.connect(
-    "mongodb+srv://fiaphackaton:1c1ETiO20VwnIkoN@cluster-fiap-hackaton-b.glhxzcn.mongodb.net/?retryWrites=true&w=majority&appName=cluster-fiap-hackaton-backend"
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_USERPASSWORD}@cluster-fiap-hackaton-b.glhxzcn.mongodb.net/?retryWrites=true&w=majority&appName=cluster-fiap-hackaton-backend`
 )
 .then(() => {
     app.listen(port, () => {
